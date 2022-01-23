e37b96d1efacd13d305cf38fc57d32c3f1056371
Roberto Luiz Souza Monteiro <roberto@souzamonteiro.com>
2022-01-23 17:02:15 -0300

    Added plural nouns table

--- build.sh
+++ build.sh
@@ -6,9 +6,14 @@ rm -rf docs/*
 #cp ../maiastudio/build/* ./js
 #cp ../maiastudio/build/maiascript.js ./bin
 
+cp src/MkPlural.maia bin/mkplural.maia
+
 cat src/Shebang.js js/libmaia.js src/MkLexer.js > build/mklexer.js
 cp build/mklexer.js bin/
-chmod 755 bin/mklexer.js
+
+chmod 755 bin/*
+
+./bin/mkplural.maia -o src/lang/en/noun_plural.txt src/lang/en/noun_singular.txt
 
 ./bin/mklexer.js -o build/Lexemes-en.json "src/lang/en/*"
 cp build/Lexemes-en.json src/
